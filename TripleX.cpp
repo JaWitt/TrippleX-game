c99555437b5cd0d35b8ca52196f48eafeb9ce473
Jacob DeWitt <47462165+JaWitt@users.noreply.github.com>
2020-05-19 16:40:41 -0500

    Create TripleX.cpp

---
+++ TripleX.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "You are a virus made to infect global computer networks.";
+	std::cout << std::endl;
+	std::cout << "Infect more networks to make yourself more powerful.";
+	std::cout << std::endl;
+	std::cout << "Be careful, the humans will catch on and make things more difficult.";
+
+	int a = 4;
+	int b = 1;
+	int c = 5;
+
+	int sum = a + b + c;
+	int product = a * b * c;
+
+	std::cout << std::endl;
+	std::cout << sum << std::endl;
+	std::cout << product << std::endl;
+	
+	return 0;
+}
